258a490227ea7af6d5cb0ef06a64882a097e2f50
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-04-28 11:23:07 +0530

    Bracket evaluator without stl initial commit

---
+++ bracket_eval_op.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Stack{
+    char *arr;
+    int top;
+    int capacity;
+
+    public:
+        Stack(int size){
+            capacity = size;
+            arr = new char[capacity];
+            top = -1;
+        }
+
+        void push(char x){
+            if(top == capacity - 1){
+                cout << "Stack Overflow" << endl;
+                return;
+            }
+            arr [++top] = x;
+        }
+
+        void pop(){
+            if(top == -1){
+                return;
+            }
+            top--;
+        }
+
+        char peek(){
+            if(top == -1){
+                cout << "Stack is empty" << endl;
+                return '%';
+            }
+            return arr[top];
+        }
+
+        bool empty(){
+            return top == -1;
+        }
+};
+
+bool valid(string s){
+    
+    Stack st(s.length());
+
+    for(char c : s){
+        
+    }
+}
+
+int main()
+{
+    return 0;
+}
