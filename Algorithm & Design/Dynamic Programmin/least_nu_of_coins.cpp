7eeba6e60f3ddddf1858fcd6c0f2d43b9b629e9d
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-07-13 12:09:02 +0530

    Least number of coin to be used when giving the change

---
+++ Algorithm & Design/Dynamic Programmin/least_nu_of_coins.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int num_coins;
+    if(!(cin >> num_coins) || num_coins <= 0){
+        cout << "Invalid input" << endl;
+        return 0;
+    }
+
+    
+}
