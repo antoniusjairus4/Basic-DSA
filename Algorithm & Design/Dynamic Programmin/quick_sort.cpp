909c190228c3be63dc693497bd0ed12dc4f61d10
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-07-27 22:07:48 +0530

    quick sort

--- Algorithm & Design/Dynamic Programmin/quick_sort.cpp
+++ Algorithm & Design/Dynamic Programmin/quick_sort.cpp
@@ -4,61 +4,18 @@
 
 using namespace std;
 
-int swaps = 0;
+int swap_counter = 0;
 
-int partition(int arr[], int low, int high) {
+int partition(int arr[], int low, int high){
     int pivot = arr[high];
     int i = low - 1;
 
-    for(int j = low; j < high; j++) {
-        if(arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
-            swaps++;
-        }
-    }
-
-    swap(arr[i], arr[high]);
-    swaps++;
-
-    return i + 1;
-}
-
-void quickSort(int arr[], int low, int high) {
-    if(low < high) {
-        int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+    for(int j = low; j < high; j++){
+        
     }
 }
 
-int main() {
-    int n;
-    cin >> n;
-
-    if(n < 0) {
-        cout << "Invalid input";
-        return 0;
-    }
-
-    int* arr = new int[n];
-
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    quickSort(arr, 0, n - 1);
-
-    for(int i = 0; i < n; i++) {
-        cout << arr[i];
-        if(i != n - 1) cout << " ";
-    }
-
-    cout << endl;
-    cout << swaps;
-
-    delete[] arr;
-
+int main()
+{
     return 0;
 }
