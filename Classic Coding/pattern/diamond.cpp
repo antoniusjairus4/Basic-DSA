b5e5e6b17af9e5a16a22578cfc9e9ebfee13a787
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-08-11 16:49:34 +0530

    Diamond

--- Classic Coding/pattern/diamond.cpp
+++ Classic Coding/pattern/diamond.cpp
@@ -8,16 +8,32 @@ int main()
     int n;
     cin >> n;
 
-    for(int i = 0; i < n; i++){
 
-        for(int j = 0; j < n - i - 1; j++){
+    for(int i = 1; i <= n; i++){
+
+        for(int j = 0; j < n - i; j++){
             cout << " ";
         }
 
         for(int k = 0; k < (2 * i - 1); k++){
-
+            cout << "*";
         }
+
+        cout << endl;
     }
 
-    
+    for(int i = n - 1; i > 0; i--){
+
+        for(int j = 0; j < n - i; j++){
+            cout << " ";
+        }
+
+        for(int k = 0; k < (2 * i - 1); k++){
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
 }
