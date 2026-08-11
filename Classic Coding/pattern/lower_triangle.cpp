d131eacb17434a5e5bd49b061b6a74b28e1e4840
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-08-11 16:33:37 +0530

    Lower triangle

---
+++ Classic Coding/pattern/lower_triangle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int i = n; i > 0; i--){
+
+        for(int j = 0; j < n; j++){
+            cout << " ";
+        }
+
+        for(int k = 0; k < (2 * i - 1); k++){
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
