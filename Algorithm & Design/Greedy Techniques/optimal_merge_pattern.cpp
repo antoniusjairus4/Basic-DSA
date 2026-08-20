903f02270da1ffb0a2a0d65a5f3398d325fa3c5d
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-08-20 14:42:23 +0530

    Optimal Merge pattern

--- Algorithm & Design/Greedy Techniques/optimal_merge_pattern.cpp
+++ Algorithm & Design/Greedy Techniques/optimal_merge_pattern.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-int main()
-{
-    return 0;
+int main() {
+    int n;
+    cin >> n;
+
+    if (n < 0) {
+        cout << "Invalid input";
+        return 0;
+    }
+
+    priority_queue <int, vector<int>, greater<int>> pq;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        pq.push(x);
+    }
+
+    int cost = 0;
+
+    while (pq.size() > 1) {
+        int a = pq.top();
+        pq.pop();
+
+        int b = pq.top();
+        pq.pop();
+
+        int sum = a + b;
+        cost += sum;
+
+        pq.push(sum);
+    }
+
+    cout << cost;
 }
